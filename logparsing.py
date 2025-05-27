cb69283f27776d0d1f8c9263af6f4a6448fc9df0
Tiberiu18 <ttiberiu48@yahoo.com>
2025-05-27 23:02:26 +0300

    Added first version of logparsing.py

---
+++ logparsing.py
@@ -0,0 +1,71 @@
+#🟦 1. Log Parser în Python
+#Scop: Script care:
+
+#Citește fișierul de log (app.log).
+
+#Filtrează doar liniile ce conțin „ERROR” sau „CRITICAL”.
+
+#Le împarte în grupuri de câte 10.
+
+#Le trimite către un endpoint /logs.
+
+#Tehnologii: Python + requests + json
+
+#📝 Pași:
+
+#Creează scriptul de citire și filtrare.
+
+#Creează o funcție care împarte lista în batch-uri.
+
+#Creează o funcție care trimite un batch la API (POST request).
+
+#Testează local cu un endpoint fals (ex: httpbin.org/post).
+
+import requests
+import json
+import argparse
+import re
+def readLogFile(filename):
+    with open(filename,"r") as logf:
+        content = logf.read()
+        return content
+
+def createListFromContent(content):
+    #Split lines with newline as separator
+    listOfLines=re.split(r'[\n]', content)
+    return listOfLines
+
+
+def createBatchesOfTen(content):
+    listOfLines=createListFromContent(content)
+    listOfBatches = []
+    count = 0
+    batch=[]
+    for line in listOfLines:
+        batch.append(line)
+        count=count+1
+        if count == 10:
+            listOfBatches.append(batch)
+            batch.clear()
+            count = 0
+    return listOfBatches
+
+#Creează o funcție care trimite un batch la API (POST request).
+def sendBatch(URL,batch):
+    payload = json.dumps(batch)
+    headers = {'Content-Type':'application/json'}
+    response = requests.post(URL, data=payload, headers=headers)
+    if response.status_code == 200:
+        print("Batch-ul a fost trimis cu success")
+    else:
+        print("Batch-ul nu a reusit sa fie trimis. eroarea este ", response.status_code)
+    return response.status_code
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Log parsing and forwarding")
+    parser.add_argument("logfile")
+    arguments = parser.parse_args()
+    logfile = arguments.logfile
+    logfile_content = readLogFile(logfile)
+    listOfBatches=createBatchesOfTen(logfile_content)
+    sendBatch(listOfBatches[0])
