736599976a4f69984b4b078fa6b36d168806d698
Tiberiu18 <ttiberiu48@yahoo.com>
2025-06-24 15:58:54 +0300

    kube basics finished

--- log-receiver-api/index.js
+++ log-receiver-api/index.js
@@ -80,5 +80,6 @@ app.get("/health", async(req,res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, console.log(`server running port ${PORT}...`))
-
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`server running on port ${PORT}...`);
+});
