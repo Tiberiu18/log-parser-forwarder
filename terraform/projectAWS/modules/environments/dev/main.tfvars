dc9222e1f3714ac8867e7e116ee2d10416a9eed9
Tiberiu18 <ttiberiu48@yahoo.com>
2025-06-29 20:13:54 +0300

    vpc refactoring

---
+++ terraform/projectAWS/modules/environments/dev/main.tfvars
@@ -0,0 +1,7 @@
+region="eu-central-1"
+environment="development"
+tags = {
+	Owner = "Tibi-DEV"
+	Project = "log-parser-forward"
+	Environment = "DEV"
+}
--- terraform/projectAWS/modules/vpc/outputs.tf
+++ terraform/projectAWS/modules/vpc/outputs.tf
@@ -1,35 +1,35 @@
 output "vpc_id" {
-	description = "VPC ID"
-	value = aws_vpc.myvpc.id
+  description = "VPC ID"
+  value       = aws_vpc.myvpc.id
 }
 
 output "public_subnet_id" {
-	description = "Public Subnet ID"
-	value = aws_subnet.public_subnet.id
+  description = "Public Subnet ID"
+  value       = aws_subnet.public_subnet.id
 }
 
 output "private_subnet_id" {
-	description = "Private subnet id"
-	value = aws_subnet.private_subnet.id
+  description = "Private subnet id"
+  value       = aws_subnet.private_subnet.id
 }
 
 output "public_route_table_id" {
-	description = "Public route table ID"
-	value = aws_route_table.public_rt.id
+  description = "Public route table ID"
+  value       = aws_route_table.public_rt.id
 }
 
 output "private_route_table_id" {
-	description = "Private route table ID"
-	value = aws_route_table.private_rt.id
-	
+  description = "Private route table ID"
+  value       = aws_route_table.private_rt.id
+
 }
 
 output "internet_gateway_id" {
-	description = "IGW ID"
-	value = aws_internet_gateway.igw.id
+  description = "IGW ID"
+  value       = aws_internet_gateway.igw.id
 }
 
 output "nat_gateway_id" {
-	description = "Nat Gateway ID"
-	value = aws_nat_gateway.nat.id
+  description = "Nat Gateway ID"
+  value       = aws_nat_gateway.nat.id
 }
